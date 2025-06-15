0c003c8d426876900783e25821538d990a0194d2
fakemoney99 <notfakemoney99@gmail.com>
2025-06-15 18:42:05 -0300

    4

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body className="light-theme">{children}</body>
     </html>
   )
 }
